0d6956f4017d2e219fc9aad55d97231ae6a83593
Wyh602683200 <155012405+Wyh602683200@users.noreply.github.com>
2024-03-16 21:57:02 +0800

    Update caihongpi.py

--- caihongpi.py
+++ caihongpi.py
@@ -23,7 +23,7 @@ class caihongpi(Plugin):
         logger.info(f"[{__class__.__name__}] inited")
 
     def get_help_text(self, **kwargs):
-        help_text = f"发送【夸我】获取摸鱼日历"
+        help_text = f"发送【夸我】获取彩虹屁"
         return help_text
 
     def on_handle_context(self, e_context: EventContext):
@@ -31,12 +31,13 @@ class caihongpi(Plugin):
         if e_context['context'].type != ContextType.TEXT:
             return
         self.content = e_context["context"].content.strip()
+        
         if self.content == "夸我":
             logger.info(f"[{__class__.__name__}] 收到消息: {self.content}")
             reply = Reply()
             result = self.caihongpi()
             if result != None:
-                reply.type = ReplyType.IMAGE_URL if type(result) == str else ReplyType.IMAGE
+                reply.type = ReplyType.TEXT
                 reply.content = result
                 e_context["reply"] = reply
                 e_context.action = EventAction.BREAK_PASS
@@ -49,20 +50,23 @@ class caihongpi(Plugin):
 
     def caihongpi(self):
         url = BASE_URL_DM + "dm-caihongpi"
-        parameter = "type=json"
+        params = "type=json"
         headers = {'Content-Type': "application/x-www-form-urlencoded"}
         try:
-            response = requests.get(url, params=parameter,headers=headers)
+            # 主接口
+            response = requests.get(url=url, params=params, headers=headers,timeout=2)
             if response.status_code == 200:
                 json_data = response.json()
-                if json_data.get('code') == 200 and json_data['data']:
-                    res = json_data['data']
-                    logger.info(json_data)
-                    return res
+                if json_data.get('code') == 200 and json_data['text']:
+                    text = json_data['data']['content']
+                    logger.info(f"主接口获取成功：{text}")
+                    return text
                 else:
-                    logger.info(json_data)
+                    logger.error(f"主接口返回值异常:{json_data}")
+                    raise ValueError('not found')
             else:
-                logger.info(f"接口异常：{response.status_code}")
+                logger.error(f"主接口请求失败:{response.text}")
+                raise Exception('request failed')
         except Exception as e:
             logger.error(f"接口异常：{e}")
                 
